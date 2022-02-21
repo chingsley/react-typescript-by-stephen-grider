bb93736eb77c1709ddb1d8b7f9620b58845a8cf8
Kingsley Eneja <eneja.kc@gmail.com>
2022-02-21 16:22:02 +0100

    - Types in a class component

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -8,8 +8,25 @@ const users = [
 ];
 
 const UserSearch: React.FC = () => {
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string, age: number } | undefined>()
+
+  const onClick = () => {
+    const foundUser = users.find(user => user.name === name);
+
+    setUser(foundUser)
+  }
+
   return (
-    <div>UserSearch</div>
+    <div>
+      User Search
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}{' '}
+        {user && user.age}
+      </div>
+    </div>
   )
 }
 
