9218a8245e5582ea67081f1fc553d94b8200451c
Kingsley Eneja <eneja.kc@gmail.com>
2022-02-23 15:26:47 +0100

    - Adding types for useRef

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom';
 // import EventComponent from './events/EventComponent';
 // import UserSearch from './state/UserSearch';
-import UserSearch from './classes/UserSearch';
+// import UserSearch from './classes/UserSearch';
+import UserSearch from './refs/UserSearch';
 
 
 const App = () => {
   return <div>
-    <UserSearch users={[{name: 'kingsley', age: 34}]} />
+    <UserSearch  />
   </div>
 };
 
