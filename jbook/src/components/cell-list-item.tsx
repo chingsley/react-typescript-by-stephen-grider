8659576b0634c43843393b99ef0fa8f59c72fdae
Kingsley Eneja <kingsley.eneja@getir.com>
2022-11-02 20:34:34 +0100

    - Implement 'add new cell' feature

--- jbook/src/components/cell-list-item.tsx
+++ jbook/src/components/cell-list-item.tsx
@@ -1,6 +1,8 @@
+import './cell-list-item.css';
 import { Cell } from '../state';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
+import ActionBar from './action-bar';
 
 interface CellListItemProps {
   cell: Cell;
@@ -10,15 +12,24 @@ const CellListItem: React.FC<CellListItemProps> = (props) => {
   const { cell } = props;
   let child: JSX.Element;
   if (cell.type === 'code') {
-    child = <CodeCell cell={cell} />;
-    // child = <div>testing</div>;
+    child = <>
+      <div className="action-bar-wrapper">
+        <ActionBar id={cell.id} />
+      </div>
+      <CodeCell cell={cell} />
+    </>;
   } else {
-    child = <TextEditor cell={cell} />;
+    child = <>
+      <TextEditor cell={cell} />
+      <ActionBar id={cell.id} />
+    </>;
   }
 
-  return <div>
-    {child}
-  </div>;
+  return (
+    <div className="cell-list-item">
+      {child}
+    </div>
+  );
 };
 
 export default CellListItem;
