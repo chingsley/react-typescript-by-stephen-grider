a209c15456c0302aab53317107dffa2aee4af453
Kingsley Eneja <kingsley.eneja@getir.com>
2022-11-13 16:54:04 +0100

    - Implement 'show' function to display output on preview window

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -19,9 +19,37 @@ const CodeCell: React.FC<CodeCellProps> = (props) => {
     const { data, order } = state.cells;
     const orderedCells = order.map(id => data[id]);
 
-    const codes = [];
+    const showFunc =       `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
+
+    var show = (value) => {
+      const root = document.querySelector('#root');
+
+      if (typeof value === 'object') {
+        if(value.$$typeof && value.props) {
+          _ReactDOM.render(value, root);
+        } else {
+          root.innerHTML = JSON.stringify(value);
+        }
+      } else {
+        root.innerHTML = value;
+      }
+    };
+  `;
+  const showFuncNoOp = 'var show = () => {}';
+    const codes = [
+
+    ];
     for(let c of orderedCells) {
-      if(c.type === 'code') codes.push(c.content);
+      if(c.type === 'code') {
+        if(c.id === cell.id) {
+          codes.push(showFunc);
+        } else {
+          codes.push(showFuncNoOp);
+        }
+        codes.push(c.content);
+      }
       if(c.id === cell.id) break;
     }
     return codes;
